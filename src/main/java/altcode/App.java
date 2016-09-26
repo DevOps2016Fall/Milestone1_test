8fd34b7aade495e70d9291bd8afa1a1bd738bab4
WeiFoo <wfu@ncsu.edu>
2016-09-26 12:30:59 -0400

    success

--- src/main/java/altcode/App.java
+++ src/main/java/altcode/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! GOGOGOOG" );
+        System.out.println( "Hello World! Success!" );
     }
 }

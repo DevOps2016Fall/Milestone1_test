bc24dd62dfb199c811d5d0e090160001841a399e
WeiFoo <wfu@ncsu.edu>
2016-09-26 13:58:28 -0400

    Test Sucess Demo

--- src/main/java/altcode/App.java
+++ src/main/java/altcode/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "We make some changes here on master branch, which goes to Success job." );
+        System.out.println( "We make some changes here on master branch, which goes to Success job!! We will test it!" );
     }
 }

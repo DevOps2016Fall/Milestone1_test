fd2ec0aa9e051ab7f4188a7e6c04dd5eed3ce2f6
WeiFoo <wfu@ncsu.edu>
2016-09-26 16:23:25 -0400

    Test success buid job

--- src/main/java/altcode/App.java
+++ src/main/java/altcode/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "We make some changes here on master branch, which goes to Success job!! We will test it!" );
+        System.out.println( "This is the java code, we're making some changes and then build it successfully I hope!!" );
     }
 }
